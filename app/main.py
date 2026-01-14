d1fa5f51e3e4d01b280d5244c55f9335194928b3
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-01-14 17:17:56 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,28 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    splitted_command = command.split(" ")
+
+    if len(splitted_command) == 3:
+        initial_file = splitted_command[1]
+        destination_file = splitted_command[2]
+
+        if (
+                splitted_command[0] == "mv"
+                and os.path.exists(initial_file)
+                and initial_file != destination_file
+        ):
+            if "/" in destination_file:
+                destination_path = destination_file.split("/")
+                current_path = destination_path[0]
+                for i in range(1, len(destination_path)):
+                    if not os.path.exists(current_path):
+                        os.mkdir(current_path)
+                    current_path += "/" + destination_path[i]
+
+            with (open(initial_file, "r") as old_file,
+                  open(destination_file, "w") as new_file):
+                new_file.write(old_file.read())
+
+            os.remove(initial_file)
