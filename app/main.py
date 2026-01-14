ced05a5969bc8538cf6ee4301ba5cc91d18205c9
OlenaShulga <oshulga1@stu.vistula.edu.pl>
2026-01-14 17:56:18 +0200

    fixed mistakes

--- app/main.py
+++ app/main.py
@@ -5,24 +5,26 @@ def move_file(command: str) -> None:
     splitted_command = command.split(" ")
 
     if len(splitted_command) == 3:
-        initial_file = splitted_command[1]
-        destination_file = splitted_command[2]
+        _, initial_file, destination_file = splitted_command
 
         if (
                 splitted_command[0] == "mv"
                 and os.path.exists(initial_file)
                 and initial_file != destination_file
         ):
-            if "/" in destination_file:
-                destination_path = destination_file.split("/")
-                current_path = destination_path[0]
-                for i in range(1, len(destination_path)):
-                    if not os.path.exists(current_path):
-                        os.mkdir(current_path)
-                    current_path += "/" + destination_path[i]
+            destination_path = destination_file.split("/")
+            current_path = "."
+            for i in range(0, len(destination_path) - 1):
+                current_path = os.path.join(current_path, destination_path[i])
+                if not os.path.exists(current_path):
+                    os.mkdir(current_path)
+
+            final_file_name = initial_file if destination_file[-1] == "/" \
+                else destination_path[-1]
 
             with (open(initial_file, "r") as old_file,
-                  open(destination_file, "w") as new_file):
+                  open(os.path.join(current_path, final_file_name), "w")
+                  as new_file):
                 new_file.write(old_file.read())
 
             os.remove(initial_file)
